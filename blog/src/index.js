c83c38e2f5434e10c2350b57fc403fd0ac04846b
vikhang2t1 <vikhangngo953@gmail.com>
2022-01-17 16:24:22 +0700

    Add Bootstrap

---
+++ blog/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { engine } = require ('express-handlebars');
+const path = require('path')
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+//Template Engine 
+app.engine('handlebars',engine());
+app.set('view engine','handlebars');
+app.set('views', path.join(__dirname,'/resource/views'));
+
+
+app.get('/',(req,res)=> res.render('home'));
+app.listen(port,()=>console.log(`Example app listening at http://localhost:${port}`));
